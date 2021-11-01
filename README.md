18f81fb042b561eca585edfa72fbe64471a5a2fd
Isha Sidhu <ishasimranxo@gmail.com>
2021-11-01 16:20:42 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Fall-CPNT262-A4 : Node Command Line App
+## By Isha Simran
+- [GitHub Repo](https://github.com/IshaSimran/fall-cpnt262-a4)
+
+Okay so I am going to be very blunt. Backend javascript absolutley terrifies me. Like I could cry just thinking about it. So here's to trying my best and probably messing up.
+
+### Commands:
+This app is used to verify the speed of an airplane, boat or car. Or at least that is what I attempted to do.
+
+- 1 - Verify the speed of a Lamborghini(car): 
+$node app drive --speed=100 --car=Lamborghini
+
+- 2 - Verify the speed of a Runabout(boat): 
+$node app sail --speed=50 --boat=Runabout
+
+- 3 - Verify the speed of a Boeing 777(airplane): 
+$node app fly --speed=1000 --airplane=Boeing-777
+
+### Attributions
+
+- [Yargs NPM Package](https://www.npmjs.com/package/yargs)
---
+++ app.js
@@ -0,0 +1,29 @@
+const yargs = require("yargs");
+console.log(yargs.argv);
+
+let command = yargs.argv._[0];
+let params = yargs.argv;
+
+if (command === 'train') {
+  console.log('pipeline');
+} else if (command === 'drive') {
+  if (params.speed && params.car) {
+    console.log(`Driving a ${params.car} at the speed of ${params.speed} km per hour`);
+  } else {
+    console.log('driving');
+  }
+} else if (command === 'sail') {
+  if (params.speed && params.boat) {
+    console.log(`Sailing a ${params.boat} at the speed of ${params.speed} km per hour`);
+  } else {
+    console.log('sailing');
+  }
+} else if (command === 'fly') {
+  if (params.speed && params.airplane) {
+    console.log(`Flying a ${params.airplane} at the speed of ${params.speed} km per hour`);
+  } else {
+    console.log('flying');
+  }
+}
+
+
