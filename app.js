b5d466798850735ec8a0bc567a54688daf08e7dc
Isha Sidhu <ishasimranxo@gmail.com>
2021-11-01 19:10:02 -0600

    changes made

--- app.js
+++ app.js
@@ -6,18 +6,21 @@ let params = yargs.argv;
 
 if (command === 'train') {
   console.log('pipeline');
+
 } else if (command === 'drive') {
   if (params.speed && params.car) {
-    console.log(`Driving a ${params.car} at the speed of ${params.speed} km per hour`);
+    console.log(`Driving in a ${params.car} at the speed of ${params.speed} km per hour`);
   } else {
     console.log('driving');
   }
+
 } else if (command === 'sail') {
   if (params.speed && params.boat) {
-    console.log(`Sailing a ${params.boat} at the speed of ${params.speed} km per hour`);
+    console.log(`Sailing on a ${params.boat} at the speed of ${params.speed} km per hour`);
   } else {
     console.log('sailing');
   }
+
 } else if (command === 'fly') {
   if (params.speed && params.airplane) {
     console.log(`Flying a ${params.airplane} at the speed of ${params.speed} km per hour`);
